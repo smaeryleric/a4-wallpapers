dd1de5a6bfc3251efc97dba2d994d5d46b8b731e
exzos <exzos@Oleksandrs-MacBook-Pro.local>
2025-04-30 14:13:52 +0200

    fix: use Python for URL encoding in generate_links.sh

--- generate_links.sh
+++ generate_links.sh
@@ -2,8 +2,12 @@
 
 BASE_URL="https://github.com/smaeryleric/a4-wallpapers/blob/main"
 
+urlencode() {
+  python3 -c "import urllib.parse, sys; print(urllib.parse.quote(sys.argv[1]))" "$1"
+}
+
 find ./images -type f | while read -r filepath; do
   clean_path="${filepath#./}"
-  encoded_path="${clean_path// /%20}"
+  encoded_path=$(urlencode "$clean_path")
   echo "$BASE_URL/$encoded_path?raw=true"
 done
