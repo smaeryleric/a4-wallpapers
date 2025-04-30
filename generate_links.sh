60a667e46778cecc19d50a8538ffa654661a7b3e
exzos <exzos@Oleksandrs-MacBook-Pro.local>
2025-04-30 13:55:32 +0200

    fix: refactor script to restrict file search to 'images' folder

--- generate_links.sh
+++ generate_links.sh
@@ -2,7 +2,7 @@
 
 BASE_URL="https://github.com/smaeryleric/a4-wallpapers/blob/main"
 
-find . -type f | while read -r filepath; do
+find ./images -type f | while read -r filepath; do
   clean_path="${filepath#./}"
   encoded_path="${clean_path// /%20}"
   echo "$BASE_URL/$encoded_path?raw=true"
